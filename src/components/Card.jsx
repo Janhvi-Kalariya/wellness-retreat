8d5d6b771ca7db6a41380d872920f5d44871ed07
Janhvi Kalariya <100296727+Janhvi-Kalariya@users.noreply.github.com>
2024-07-27 20:20:58 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,10 @@ import React, { useState, useEffect, useRef } from 'react';
 const Card = (props) => {
     const { img, title, description, date, location, price } = props;
     function formatDate(unixTimestamp) {
-        const date = new Date(unixTimestamp * 1000); // Convert Unix timestamp to milliseconds
+        const date = new Date(unixTimestamp * 1000);
         const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+        const month = String(date.getMonth() + 1).padStart(2, '0'); 
         const day = String(date.getDate()).padStart(2, '0');
-        // const hours = String(date.getHours()).padStart(2, '0');
-        // const minutes = String(date.getMinutes()).padStart(2, '0');
-        // const seconds = String(date.getSeconds()).padStart(2, '0');
-        // console.log(date,"date")
-      
         return `${day}-${month}-${year}`;
       }
     return(
